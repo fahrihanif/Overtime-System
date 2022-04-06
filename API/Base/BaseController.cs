5320324ab0b375f331d802d0540148c2197066cd
Fahri Hanif <fahrihanif03@gmail.com>
2022-04-06 09:27:22 +0700

    Implement general repository pattern

---
+++ API/Base/BaseController.cs
@@ -0,0 +1,107 @@
+﻿using API.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Base
+{
+    public class BaseController<Entity, Repository, Key> : ControllerBase
+        where Entity : class
+        where Repository : IRepository<Entity, Key>
+    {
+        private readonly Repository repository;
+
+        public BaseController(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        //Get Data
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            try
+            {
+                var get = repository.Get().Count();
+                return get == 0
+                    ? NotFound(new { message = "Data Empty" })
+                    : (ActionResult)Ok(repository.Get());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        //Get Data with parameter primary key
+        [HttpGet("{id}")]
+        public ActionResult GetById(Key id)
+        {
+            try
+            {
+                var get = repository.Get(id);
+                return get == null
+                    ? NotFound(new { message = "Data Not Found" })
+                    : (ActionResult)Ok(get);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+
+        }
+
+        //Insert data to entity
+        [HttpPost]
+        public virtual ActionResult Post(Entity entity)
+        {
+            try
+            {
+                var post = repository.Insert(entity);
+                return post == 0
+                    ? NotFound(new { message = "Data Failed to Change Please Check Again" })
+                    : (ActionResult)Ok(new { message = "Data Saved Successfully" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        //Update data from existing row in entity
+        [HttpPut]
+        public virtual ActionResult Update(Entity entity)
+        {
+            try
+            {
+                var update = repository.Update(entity);
+                return update == 0
+                    ? NotFound((new { message = "Data Failed to Change Please Check Again" }))
+                    : (ActionResult)Ok(new { message = "Data Has Been Successfully Changed" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        //Delete data from row in entity by primary key
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Key id)
+        {
+            try
+            {
+                var delete = repository.Delete(id);
+                return delete == 0
+                    ? NotFound(new { message = $"{id} Not Found" })
+                    : (ActionResult)Ok(new { message = $"Your selected id has been deleted" });
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+    }
+}
---
+++ API/Repository/GeneralRepository.cs
@@ -0,0 +1,65 @@
+﻿using API.Contexts;
+using API.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repository
+{
+    public class GeneralRepository<Context, Entity, Key> : IRepository<Entity, Key>
+        where Entity : class
+        where Context : MyContext
+    {
+        private readonly MyContext myContext;
+        private readonly DbSet<Entity> entities;
+
+        public GeneralRepository(MyContext myContext)
+        {
+            this.myContext = myContext;
+            entities = myContext.Set<Entity>();
+        }
+
+        //This method used to delete row in entity by Primary Key
+        public int Delete(Key key)
+        {
+            var check = entities.Find(key);
+            if (check != null)
+            {
+                entities.Remove(check);
+                var result = myContext.SaveChanges();
+                return result;
+            }
+            return 0;
+        }
+
+        //This method used to get all row in entity
+        public IEnumerable<Entity> Get()
+        {
+            return entities.ToList();
+        }
+
+        //This method used to get a row in entity by Primary Key
+        public Entity Get(Key key)
+        {
+            return entities.Find(key);
+        }
+
+        //This method used to insert a row to entity
+        public int Insert(Entity entity)
+        {
+            entities.Add(entity);
+            var result = myContext.SaveChanges();
+            return result;
+        }
+
+        //This method used to update a row in entity
+        public virtual int Update(Entity entity)
+        {
+            myContext.Entry(entity).State = EntityState.Modified;
+            var result = myContext.SaveChanges();
+            return result;
+        }
+    }
+}
