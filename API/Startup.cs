817b24309800e02dfd36c610c5b3524858da6bd2
Fahri Hanif <fahrihanif03@gmail.com>
2022-04-06 09:35:42 +0700

    Last step

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,14 +1,20 @@
+using API.Contexts;
+using API.Repository.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace API
@@ -26,6 +32,44 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Adds a scoped service of the type specified in Repository.Data
+            services.AddScoped<RoleRepository>();
+            services.AddScoped<JobRepository>();
+            services.AddScoped<AccountRepository>();
+            services.AddScoped<EmployeeRepository>();
+            services.AddScoped<OvertimeRepository>();
+            services.AddScoped<AccountRoleRepository>();
+            services.AddScoped<EmployeeOvertimeRepository>();
+
+            //This is to configures MyContext to connect to a SSMS
+            //"Connection" has created in appsetings.json
+            services.AddDbContext<MyContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("Connection")));
+
+            //This is to handled reference loop eror to Json format
+            services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            //This is to configuration JWT
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,9 +80,13 @@ namespace API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors();
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
