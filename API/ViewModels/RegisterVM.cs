7bf8f2f27925367bc8549ee9f2a5bf0896b0c41a
Fahri Hanif <fahrihanif03@gmail.com>
2022-04-06 09:26:41 +0700

    Add view model

---
+++ API/ViewModels/RegisterVM.cs
@@ -0,0 +1,21 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModels
+{
+    public class RegisterVM
+    {
+        public string NIK { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public Gender Gender { get; set; }
+        public int Salary { get; set; }
+        public int JobId { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
