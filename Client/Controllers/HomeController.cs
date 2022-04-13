290e8e5fd01bf631238698adbc25d03eadba108b
Fahri Hanif <fahrihanif03@gmail.com>
2022-04-13 14:12:08 +0700

    Add login view

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -11,27 +11,19 @@ namespace Client.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult ForgotPassword()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult ChangePassword()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
